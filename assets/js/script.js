d31d99f57976fb0ec5b0d05562a04a2e6b0b7dc2
Christo107 <christopher.odwyer82@gmail.com>
2022-10-11 16:08:02 +0000

    add basic javascript

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+    
+}
+
+function incrementScore() {
+    
+}
+
+function incrementWrongAnswer() {
+    
+}
+
+function disaplayAdditionQuestion() {
+    
+}
+
+function disaplaySubtractionQuestion() {
+    
+}
+
+function disaplayMultiplyQuestion() {
+    
+}
+
+function disaplayDivisionQuestion() {
+    
+}
